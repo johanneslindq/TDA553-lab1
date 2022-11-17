a0f9e9e7e3a56a386ad013975adc3501f952af6e
johanneslindqv <johanneslindqvist24@gmail.com>
2022-11-17 15:13:30 +0100

    modified:   Saab95.java

--- Saab95.java
+++ Saab95.java
@@ -3,7 +3,6 @@ import java.awt.*;
 public class Saab95 extends car{
 
     private boolean turboOn;
-    
     public Saab95(){
         super(2, Color.red, 125, "Saab95");
 	    this.turboOn = false;
